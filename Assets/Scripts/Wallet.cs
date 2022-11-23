2ee4c5c3231a704c9ae754117b5d9b32e1659ec6
Artem <jakmobius@gmail.com>
2022-11-23 17:41:49 +0300

    Remove MonoBehaviour inheritance from Wallet singleton; add event system to Wallet

--- Assets/Scripts/Wallet.cs
+++ Assets/Scripts/Wallet.cs
@@ -1,38 +1,27 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using TMPro;
 
-public class Wallet : MonoBehaviour{
-    public TMP_Text title;
-
-    public static Wallet Instance;
-    private int money = 40/*0*/; // для тестов
-
-    private void Awake() {
-        Instance = this;
-        DontDestroyOnLoad(gameObject);
-    }
-
-    private void Start() {
-        Draw();
-    }
-
-    private void Draw() {
-        title.text = string.Format("Money: {0}", Count());
-    }
+public class Wallet {
+    public static Wallet Instance = new Wallet();
+    private int _money = 10;
+    public event Action<int> OnMoneyChanged;
 
     public int Count() {
-        return money;
+        return _money;
     }
     
-    public void Add(int cnt) {
-        money += cnt;
-        Draw();
+    public void Add(int cnt)
+    {
+        _money += cnt;
+        if(OnMoneyChanged != null)
+            OnMoneyChanged(_money);
     }
 
     public void Dec(int cnt) {
-        money -= cnt;
-        Draw();
+        _money -= cnt;
+        if(OnMoneyChanged != null)
+            OnMoneyChanged(_money);
     }
 }
