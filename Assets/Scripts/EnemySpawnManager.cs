fb867e53e35ad5675ed348d11aae8d9339f6e495
loochek <m.ziganshin.zm@gmail.com>
2022-12-31 23:32:30 +0300

    Fix build error

--- Assets/Scripts/EnemySpawnManager.cs
+++ Assets/Scripts/EnemySpawnManager.cs
@@ -2,7 +2,6 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
-using UnityEditor.TerrainTools;
 using UnityEngine;
 
 public class EnemySpawnManager : MonoBehaviour
